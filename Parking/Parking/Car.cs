6b7a245ca17e2e26ff93d799d6789c9974ba026d
vladlutsenko1996 <vladlutsenko1996@gmail.com>
2018-05-10 11:41:52 +0300

    add class Car

---
+++ Parking/Parking/Car.cs
@@ -0,0 +1,36 @@
+﻿using Parking.Enums;
+using System;
+
+namespace Parking
+{
+    internal class Car
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+            }
+        }
+
+        public Guid Id { get; set; }
+
+        public CarType CarType { get; set; }
+
+        public Car(decimal balance, CarType carType)
+        {
+            Id = Guid.NewGuid();
+            Balance = balance;
+            CarType = carType;
+        }
+    }
+}
