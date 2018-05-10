5a40ab9302a098768b9b5865e8b3c28f1d9b85ff
vladlutsenko1996 <vladlutsenko1996@gmail.com>
2018-05-10 11:51:44 +0300

    realize class Transaction

---
+++ Parking/Parking/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    internal class Transaction
+    {
+        public DateTime Date_Time { get; set; }
+
+        public Guid CarId { get; set; }
+
+        public decimal WriteOffs { get; set; }
+
+        public Transaction(Guid carId, decimal writeOffs)
+        {
+            CarId = carId;
+            WriteOffs = writeOffs;
+            Date_Time = DateTime.Now;
+        }
+    }
+}
