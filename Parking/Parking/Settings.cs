c36d30f8fdb563ee86b8785d33aa97f32d0cc780
vladlutsenko1996 <vladlutsenko1996@gmail.com>
2018-05-10 11:15:02 +0300

    add enum CarType and class Settings
    CarType - enum, for different cars
    Setting - class, for customizing parking(Singleton)

---
+++ Parking/Parking/Settings.cs
@@ -0,0 +1,49 @@
+﻿using Parking.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    /// <summary>
+    /// Customizing parking data
+    /// </summary>
+    internal class Settings
+    {
+        /// <summary>
+        /// Every Timeout seconds charges money for parking space
+        /// </summary>
+        public int Timeout { get; private set; }
+
+        /// <summary>
+        /// Prices for parking for all cars
+        /// </summary>
+        public Dictionary<CarType, decimal> Prices { get; private set; }
+
+        /// <summary>
+        /// Amount of parking spaces
+        /// </summary>
+        public int ParkingSpace { get; private set; }
+
+        /// <summary>
+        /// Coefficient of fine
+        /// </summary>
+        public double Fine { get; private set; }
+
+        private static readonly Lazy<Settings> lazy = new Lazy<Settings>(() => new Settings());
+
+        public static Settings Instance => lazy.Value;
+        
+        private Settings() { }
+
+        public Settings(Dictionary<CarType, decimal> prices, int parkingSpace, double fine, int timeout = 3)
+        {
+            Prices = prices;
+            ParkingSpace = parkingSpace;
+            Fine = fine;
+            Timeout = timeout;
+        }
+    }
+}
